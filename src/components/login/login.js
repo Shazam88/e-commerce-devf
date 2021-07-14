e853a94ffafa4fa7ecc8e9510b3049bd15a7a5b6
MiguelM <miguelmonterroso39@gmail.com>
2021-07-13 19:13:36 -0600

    login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,7 +4,6 @@ import image from './images/image-login.jpg'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
-import { Link } from 'react-router-dom';
 
 const MySwal = withReactContent(Swal)
 
@@ -65,7 +64,7 @@ export const Login = () => {
                 <button type="submit" disabled={isSubmitting}>
                     Enviar
                 </button>
-                <p>¿Aún no tienes cuenta? <Link to='/sign-up'>Registrate aquí</Link></p>
+                <p>¿Aún no tienes cuenta? Registrate aquí</p>
                 </Form>
                 )}
         </Formik>
